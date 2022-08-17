9676a71c6847e13a5f90ad41f477e7d2c7f2419a
Vyacheslav <g4linoff@gmail.com>
2022-08-18 01:09:25 +0700

    ds

--- golang/7th_les/main.go
+++ golang/7th_les/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import "fmt"
 
+type User struct {
+	name   string
+	age    int
+	sex    string
+	weight int
+	height int
+}
+
+func (u User) printUserInfo() {
+	fmt.Println(u.name, u.age, u.height, u.sex, u.sex)
+}
+
+type DumbDatabase struct {
+	m map[string]string
+}
+
+func NewDumbDatabase() *DumbDatabase {
+	return &DumbDatabase{
+		m: make(map[string]string),
+	}
+}
+
+func NewUser(name, sex string, age, weight, height int) User {
+	return User{
+		name:   name,
+		age:    age,
+		sex:    sex,
+		weight: weight,
+		height: height,
+	}
+}
+
+func main() {
+	user1 := NewUser("Vasya", "Male", 23, 75, 185)
+	user2 := User{"Vasya", 23, "Male", 84, 195}
+
+	fmt.Println(user1.name, user1.age)
+
+	user1.printUserInfo()
+	user2.printUserInfo()
 }
